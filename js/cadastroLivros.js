6e6e81bd27398440414f2ef45e20f452e16d2e97
igorSoares7 <igorsoaresr_contato@hotmail.com>
2024-09-10 17:25:15 -0300

    Adiciona o cadastro de livros com o method POST

---
+++ js/cadastroLivros.js
@@ -0,0 +1,34 @@
+URL_API_LIVROS = "https://api-aula.up.railway.app/livros"
+
+async function iniciarCadastro(){
+
+    // 1 entrada de dados
+    // preciso pegar os valores dos inputs
+    const title = document.getElementById("title").value
+    const description = document.getElementById("description").value
+
+
+    // 1.1 validacao dos campos
+
+    
+    if(title == "" || description == "") {
+        alert("Digite um livro e descrição!")
+    }
+
+    // 2 processamento
+    // chamada de API Construção da Request
+
+    const options =  {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify({
+            title: title,
+            description: description
+        })
+    }
+    const response = await fetch(URL_API_LIVROS, options)
+    // 3 Monstrar em tela o resultado da chamada
+    // Mensagem de sucesso ou erro
+}
